1e73a47da4c4e7657222b45735b528f85778a541
ankitpakhale <akp3067@gmail.com>
2022-03-13 00:45:40 +0530

    getting data

--- data/views.py
+++ data/views.py
@@ -5,25 +5,13 @@ import csv
 import pandas
 # Create your views here.
 
-def index(request):
-    print("1st")
-    if request.method == 'POST':
-        
-        data_frame = pandas.read_csv('./01.csv')
-        print(data_frame)
-        with open('./01.csv') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                p = Entry(pname=row['Product name'], pqty=row['product qty'],pprice=row['product price'])
-                print(p)
+def index(request):    
+    if request.POST:
+        f = request.POST['file']
+        with open(f, 'r') as f:
+            a1 = f.readlines()
+            a = a1[1::]
+            for i in a:
+                print(i[0])
 
     return render(request, 'index.html')
-
-# def CSV(request):
-#     # data_frame = pandas.read_csv('app1/Static/Data.csv')
-#     with open('app1/Static/Data.csv') as csvfile:
-#         reader = csv.DictReader(csvfile)
-#         for row in reader:
-#             p = Entry(name=row['Name'], address=row['Address'],email=row['Email'],m_no = row['M_No'])
-#             p.save()
-#     return HttpResponse("Hello World")
