88c265162a1b7044c0aa991e90cf03b4087b47f4
ankitpakhale <akp3067@gmail.com>
2022-03-12 22:51:33 +0530

    migration app commit

--- data/views.py
+++ data/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.http  import HttpResponse
+from .models import *
+import csv
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Inside data save from csv")
+    f = request.POST['file']
+    print(f)
+    
+    return render(request, 'index.html')
+
+# def CSV(request):
+#     # data_frame = pandas.read_csv('app1/Static/Data.csv')
+#     with open('app1/Static/Data.csv') as csvfile:
+#         reader = csv.DictReader(csvfile)
+#         for row in reader:
+#             p = Entry(name=row['Name'], address=row['Address'],email=row['Email'],m_no = row['M_No'])
+#             p.save()
+#     return HttpResponse("Hello World")
